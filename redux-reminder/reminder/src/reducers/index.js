21da64e1ddc1013b8349436a51051b17ab41f5ab
Tejas Parab <tejasparab890@gmail.com>
2018-03-16 20:43:57 -0400

    reminder app learning basic redux

---
+++ redux-reminder/reminder/src/reducers/index.js
@@ -0,0 +1,35 @@
+import {ADD_REMINDER, DELETE_REMINDER} from '../constants';
+
+const reminder = (action) => {
+  let {text, dueDate} = action;
+  return{
+    id: Math.random(),
+    text,
+    dueDate
+  }
+}
+
+const removeById = (state= [], id) => {
+  const reminders = state.filter(reminder => reminder.id !== id);
+  console.log('new reduced reminders', reminders);
+  return reminders;
+}
+
+
+const reminders = (state= [], action) => {
+  let reminders = null;
+  switch(action.type) {
+    case ADD_REMINDER:
+    reminders = [...state, reminder(action)]
+    console.log('reminders as state', reminders);
+    return reminders;
+    case DELETE_REMINDER:
+    reminders = removeById(state, action.id);
+    return reminders;
+    default:
+    return state;
+
+  }
+}
+
+export default reminders;
